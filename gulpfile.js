d481c84160afe6fe7cd521c124c90fcab7a4915c
AABorisov <borisov.andrew.a@gmail.com>
2018-10-07 02:56:29 +0300

    gulp watchify

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,32 @@ var gulp = require("gulp");
 var browserify = require("browserify");
 var source = require("vinyl-source-stream");
 var tsify = require("tsify");
+var watchify = require("watchify");
+var gutil = require("gulp-util");
 var paths = {
     pages: ['src/*.html']
 };
+
+var watchedBrowserify = watchify(browserify({
+    basedir: '.',
+    debug: true,
+    entries: ['src/main.ts'],
+    cache: {},
+    packageCache: {}
+}).plugin(tsify));
 
 gulp.task("copy-html", function () {
     return gulp.src(paths.pages)
         .pipe(gulp.dest("dist"));
 });
 
-gulp.task("default", ["copy-html"], function () {
-    return browserify({
-        basedir: '.',
-        debug: true,
-        entries: ['src/main.ts'],
-        cache: {},
-        packageCache: {}
-    })
-        .plugin(tsify)
+function bundle() {
+    return watchedBrowserify
         .bundle()
         .pipe(source('bundle.js'))
         .pipe(gulp.dest("dist"));
-});
+}
+
+gulp.task("default", ["copy-html"], bundle);
+watchedBrowserify.on("update", bundle);
+watchedBrowserify.on("log", gutil.log);
