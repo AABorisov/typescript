da844ed4dfc1136f352dc5f58413eddb682f25b4
AABorisov <borisov.andrew.a@gmail.com>
2018-10-08 23:44:51 +0300

    checking some abilities of typescript

--- greeter.js
+++ greeter.js
@@ -12,4 +12,103 @@ function greeter(person) {
     return "Hello, " + person.firstName + " " + person.lastName;
 }
 var user = new Student("Jane", "M.", "User");
-document.body.innerHTML = greeter(user);
+var deck = {
+    suits: ["hearts", "spades", "clubs", "diamonds"],
+    cards: Array(52),
+    // NOTE: The function now explicitly specifies that its callee must be of type Deck
+    createCardPicker: function () {
+        var _this = this;
+        return function () {
+            var pickedCard = Math.floor(Math.random() * 52);
+            var pickedSuit = Math.floor(pickedCard / 13);
+            return { suit: _this.suits[pickedSuit], card: pickedCard % 13 };
+        };
+    }
+};
+var cardPicker = deck.createCardPicker();
+var pickedCard = cardPicker();
+var Handler = /** @class */ (function () {
+    function Handler() {
+        this.onClickBad = function (e) {
+            // oops, used this here. using this callback would crash at runtime
+            // this.info = e.type;
+            console.log(e.target);
+        };
+    }
+    return Handler;
+}());
+var h = new Handler();
+var uiElement;
+// uiElement.addClickListener(h.onClickBad); // error!
+var suits = ["hearts", "spades", "clubs", "diamonds"];
+function pickCard(x) {
+    // Check to see if we're working with an object/array
+    // if so, they gave us the deck and we'll pick the card
+    if (typeof x == "object") {
+        var pickedCard_1 = Math.floor(Math.random() * x.length);
+        return pickedCard_1;
+    }
+    // Otherwise just let them pick the card
+    else if (typeof x == "number") {
+        var pickedSuit = Math.floor(x / 13);
+        return { suit: suits[pickedSuit], card: x % 13 };
+    }
+}
+var myDeck = [{ suit: "diamonds", card: 2 }, { suit: "spades", card: 10 }, { suit: "hearts", card: 4 }];
+var pickedCard1 = myDeck[pickCard(myDeck)];
+// alert("card: " + pickedCard1.card + " of " + pickedCard1.suit);
+var pickedCard2 = pickCard(15);
+// alert("card: " + pickedCard2.card + " of " + pickedCard2.suit);
+function identity(arg) {
+    return arg;
+}
+var output = identity(1);
+var E;
+(function (E) {
+    E[E["X"] = 0] = "X";
+    E[E["Y"] = 1] = "Y";
+    E[E["Z"] = 2] = "Z";
+})(E || (E = {}));
+var E2;
+(function (E2) {
+    E2["X"] = "x";
+    E2["Y"] = "y";
+    E2["Z"] = "zzz";
+})(E2 || (E2 = {}));
+var Enum3;
+(function (Enum3) {
+    Enum3[Enum3["A"] = 1] = "A";
+    Enum3[Enum3["B"] = 2] = "B";
+    Enum3[Enum3["C"] = 2] = "C";
+})(Enum3 || (Enum3 = {}));
+var b = Enum3.B;
+var nameOfB = Enum3[b];
+var c = Enum3.C;
+var nameOfC = Enum3[c];
+console.log(nameOfB, nameOfC);
+var people;
+people.value.name = "Andrey";
+var l = people.left;
+l.value;
+var BasicCalculator = /** @class */ (function () {
+    function BasicCalculator(value) {
+        if (value === void 0) { value = 0; }
+        this.value = value;
+    }
+    BasicCalculator.prototype.currentValue = function () {
+        return this.value;
+    };
+    BasicCalculator.prototype.add = function (operand) {
+        this.value += operand;
+        return this;
+    };
+    BasicCalculator.prototype.multiply = function (operand) {
+        this.value *= operand;
+        return this;
+    };
+    return BasicCalculator;
+}());
+var v = new BasicCalculator(2)
+    .multiply(5)
+    .add(1)
+    .currentValue();
--- greeter.ts
+++ greeter.ts
@@ -18,4 +18,154 @@ function greeter(person: Person) {
 
 let user = new Student("Jane", "M.", "User");
 
-document.body.innerHTML = greeter(user);
+// document.body.innerHTML = greeter(user);
+
+
+interface Card {
+    suit: string;
+    card: number;
+}
+interface Deck {
+    suits: string[];
+    cards: number[];
+    createCardPicker(this: Deck): () => Card;
+}
+let deck: Deck = {
+    suits: ["hearts", "spades", "clubs", "diamonds"],
+    cards: Array(52),
+    // NOTE: The function now explicitly specifies that its callee must be of type Deck
+    createCardPicker: function(this: Deck) {
+        return () => {
+            let pickedCard = Math.floor(Math.random() * 52);
+            let pickedSuit = Math.floor(pickedCard / 13);
+
+            return {suit: this.suits[pickedSuit], card: pickedCard % 13};
+        }
+    }
+}
+
+let cardPicker = deck.createCardPicker();
+let pickedCard = cardPicker();
+
+// alert("card: " + pickedCard.card + " of " + pickedCard.suit);
+
+interface UIElement {
+    addClickListener(onclick: (this: void, e: Event) => void): void;
+}
+
+class Handler {
+    info: string;
+    onClickBad = (e:Event) => {
+        // oops, used this here. using this callback would crash at runtime
+        // this.info = e.type;
+        console.log(e.target);
+    }
+}
+let h = new Handler();
+let uiElement: UIElement;
+// uiElement.addClickListener(h.onClickBad); // error!
+
+
+let suits = ["hearts", "spades", "clubs", "diamonds"];
+
+function pickCard(x: {suit: string; card: number; }[]): number;
+function pickCard(x: number): {suit: string; card: number; };
+function pickCard(x): any {
+    // Check to see if we're working with an object/array
+    // if so, they gave us the deck and we'll pick the card
+    if (typeof x == "object") {
+        let pickedCard = Math.floor(Math.random() * x.length);
+        return pickedCard;
+    }
+    // Otherwise just let them pick the card
+    else if (typeof x == "number") {
+        let pickedSuit = Math.floor(x / 13);
+        return { suit: suits[pickedSuit], card: x % 13 };
+    }
+}
+
+let myDeck = [{ suit: "diamonds", card: 2 }, { suit: "spades", card: 10 }, { suit: "hearts", card: 4 }];
+let pickedCard1 = myDeck[pickCard(myDeck)];
+// alert("card: " + pickedCard1.card + " of " + pickedCard1.suit);
+
+let pickedCard2 = pickCard(15);
+// alert("card: " + pickedCard2.card + " of " + pickedCard2.suit);
+
+
+function identity<T>(arg: T): T {
+    return arg;
+}
+let output = identity(1);
+
+enum E {
+    X, Y, Z
+}
+
+enum E2 {
+    X = "x", Y = "y", Z = "zzz"
+}
+
+const enum Enum {
+    A = 1,
+    B = A * 2
+}
+
+const enum Directions {
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+declare enum Enum2 {
+    A = 1,
+    B,
+    C = 2
+}
+enum Enum3 {
+    A = 1,
+    B,
+    C = 2
+}
+let b = Enum3.B;
+let nameOfB = Enum3[b];
+let c = Enum3.C;
+let nameOfC = Enum3[c];
+console.log(nameOfB, nameOfC);
+
+
+type Tree<T> = {
+    value: T;
+    left: Tree<T>;
+    right: Tree<T>;
+}
+interface Person {
+    name: string
+}
+
+var people: Tree<Person>;
+people.value.name = "Andrey";
+var l = people.left;
+l.value;
+
+
+class BasicCalculator {
+    public constructor(protected value: number = 0) { }
+    public currentValue(): number {
+        return this.value;
+    }
+    public add(operand: number): this {
+        this.value += operand;
+        return this;
+    }
+    public multiply(operand: number): this {
+        this.value *= operand;
+        return this;
+    }
+    // ... other operations go here ...
+}
+
+let v = new BasicCalculator(2)
+    .multiply(5)
+    .add(1)
+    .currentValue();
